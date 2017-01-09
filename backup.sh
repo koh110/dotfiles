ce392f3c087b4cc8d43c95fff00d410d26ffa8f0
koh110 <kohta110@gmail.com>
2017-01-09 19:11:16 +0800

    add atom backup

---
+++ backup.sh
@@ -0,0 +1,1 @@
+apm list -bi > atomfile
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-cp .ssh/* ~/.ssh
+# cp .ssh/* ~/.ssh
 cp .gitconfig ~/
 cp .vimrc ~/
 cp .tmux.conf ~/
 cp .zshrc ~/
 cp .zshenv ~/
 cp .atom ~/
+apm install --packages-file atomfile
