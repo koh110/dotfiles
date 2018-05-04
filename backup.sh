54611e2eef0a257909fd23d68ac256e600831b05
kohta ito <kohta110@gmail.com>
2018-05-05 00:17:14 +0900

    add vscode backup

--- backup.sh
+++ backup.sh
@@ -1,1 +1,6 @@
 apm list -bi > atomfile
+
+code --list-extensions > "code/extensions.txt"
+cp $HOME/Library/Application\ Support/Code/User/settings.json code
+cp $HOME/Library/Application\ Support/Code/User/keybindings.json code
+
