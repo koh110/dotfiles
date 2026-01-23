a9da95fefa5b8aef9483b298442886f7aa401cf3
kohta ito <kohta110@gmail.com>
2026-01-23 14:36:54 +0900

    fix

--- backup.ts
+++ backup.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --experimental-strip-types
+#!/usr/bin/env node
 // ./backup.ts --all
 // ./backup.ts --vim --tmux
 
--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --experimental-strip-types
+#!/usr/bin/env node
 // ./deploy.ts --all
 
 import { homedir } from 'node:os'
